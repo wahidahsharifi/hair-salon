c5a14a13d2001c7b916935262e4b9de77e8437a7
wahidAhmad <thesharifi.wahid@gmail.com>
2024-08-12 13:03:08 +0430

    lookbook
    i did lookbook using grid

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // gallery
-const img = document.querySelectorAll('.album > img')
-const album = document.querySelector('.album')
-img.forEach(x => {
-   x.addEventListener('click', () => {
-      x.classList.add('active')
-      album.classList.add('clicked')
-   })
+const img = document.querySelectorAll(".album > img");
+const album = document.querySelector(".album");
+img.forEach((x) => {
+   x.addEventListener("click", () => {
+      x.classList.add("active");
+      album.classList.add("clicked");
+   });
    // x.addEventListener('mouseout', () => {
    //    x.classList.remove('active')
    // })
-})
+});
