b7e0f8d2045ae8c335d9efec4ce8c7f6d2bd1d99
wahid sharifi <thesharifi.wahid@gmail.com>
2024-11-29 13:04:35 +0430

    lookbook image zoom
    lookbook images zoom in on click now

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 // gallery
 const img = document.querySelectorAll(".album > img");
 const album = document.querySelector(".album");
-img.forEach((x) => {
-   x.addEventListener("click", () => {
-      x.classList.add("active");
-      album.classList.add("clicked");
+const body = document.querySelector("body");
+img.forEach((image) => {
+   image.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (image.classList.contains("active")) {
+         removeClick();
+      } else {
+         image.classList.add("active");
+         album.classList.add("clicked");
+         body.classList.add("body");
+         console.log(e.target.src);
+         album.background = 'url(img/Gallery-01.jpg)'
+      }
    });
-   // x.addEventListener('mouseout', () => {
-   //    x.classList.remove('active')
-   // })
+   album.addEventListener("click", (e) => {
+      e.stopPropagation();
+      removeClick();
+   });
+   function removeClick() {
+      image.classList.remove("active");
+      album.classList.remove("clicked");
+      body.classList.remove("body");
+   }
 });
